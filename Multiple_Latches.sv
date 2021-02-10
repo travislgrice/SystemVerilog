0db7b37c16cd5861df047726a08ea423612ad1fe
Travis L Grice <travislgrice@gmail.com>
2021-02-10 12:04:09 -0600

    Update Multiple_Latches.sv
    Including a reset signal to reset the latches on assertion.

--- Multiple_Latches.sv
+++ Multiple_Latches.sv
@@ -1,15 +1,20 @@
 `timescale 1ns / 1ps
 
 // This module latches multiple bits
-module Latches(set, clk, out);
+module Latches(set, reset, clk, out);
     input logic [9:0] set;
+    input logic reset;
     input logic clk;
     output logic [9:0] out;
     always_latch begin
-        if (clk) begin
-            out <= set;
+        if (reset) begin
+            out <= 10'h000;
         end else begin
-            out <= out;
+            if (clk) begin
+                out <= set;
+            end else begin
+                out <= out;
+            end
         end
     end
 endmodule: Latches
@@ -19,10 +24,10 @@ module Latches_Testbench;
 
     // define signals
     logic [9:0] set, out;
-    logic clk;
+    logic clk, reset;
     
     // instantiate latches
-    Latches latches(.set(set), .out(out), .clk(clk));
+    Latches latches(.set(set), .reset(reset), .out(out), .clk(clk));
     
     // generate clock
     initial begin
@@ -65,6 +70,16 @@ module Latches_Testbench;
         end
         #100;
         
+        // test reset
+        // latch keys 13579 (even numbers)
+        for (int i = 1; i < 10; i+=2) begin
+            set[i] = 1'b1;
+        end
+        #100;
+        reset = 1'b1;
+        #20;
+        reset = 1'b0;
+        #100;
         $finish;
     end
     
